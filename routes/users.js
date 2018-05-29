5cffe56d7bd5aaaa49ca30e703a9a67992e1d83c
Olivia Lemmelin <olivialemmelin@gmail.com>
2018-05-29 13:54:50 -0700

    Adding parts of Authentication.

---
+++ routes/users.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+const sqlite = require('sqlite3').verbose();
+var models = require('../models');
+const passport = require("passport");
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get("/signup", function (req, res, next) {
+  res.render('signup')
+});
+
+router.post('/signup', function (req, res, next) {
+  models.users.findOrCreate({
+    where: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    }
+  }).spread(function (result, created) {
+    if (created) {
+      res.redirect('profile/' + result.UserId)
+    } else {
+      res.send('this user already exists')
+    }
+  });
+
+});
+
+router.get('/profile/:id', passport.authenticate('local', {
+  failureRedirect: '/users/login'
+}), function (req, res, next) {
+  if (req.user.UserId === req.params.id) {
+    res.render('profile', {
+      FirstName: req.user.firstName,
+      LastName: req.user.lastName,
+      Email: req.user.email,
+      UserId: req.user.UserId,
+      Username: req.user.username
+    });
+  } else {
+    res.send('This is not your profile')
+  }
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  failureRedirect: '/login'
+}), function (req, res, next) {
+  console.log(req.user)
+  res.redirect('profile/' + req.user.UserId)
+})
+
+
+module.exports = router;
